d32819e674642047e270e1f8df24a4864f60c1b6
huang12zheng <805104533@qq.com>
2022-06-23 19:19:22 +0800

    feat: make images as list(dict)

--- get_images.py
+++ get_images.py
@@ -18,14 +18,20 @@ class ConfigYaml(metaclass=YAMLObject):
         ConfigYaml.install_file_cache.append(input)
 
     def expand_images(self,input):
-        ConfigYaml.images.extend(input)
-        ConfigYaml.images = list(set(ConfigYaml.images))
+        def hash_key(obj):
+            return f"{obj['s_image']}{obj['t_image']}"
+        # ConfigYaml.images.extend(input)
+        input_diff = [ x for x in input
+            if not hash_key(x) in 
+                map(hash_key,ConfigYaml.images)
+        ]
+        ConfigYaml.images.extend(input_diff)
 
     def save_yaml(self):
         # configYamlFile=open(configYamlPath, 'w')
-        with open(configYamlPath+'1', 'w') as configYamlFile:
+        with open(f'1{configYamlPath}', 'w') as configYamlFile:
             ConfigYaml.install_file_cache = list(set(ConfigYaml.install_file_cache))
-            ConfigYaml.images = list(set(ConfigYaml.images))
+            # ConfigYaml.images = list(set(ConfigYaml.images))
 
             yaml.dump(to_cycle_dict(ConfigYaml), configYamlFile)
             configYamlFile.close()
@@ -47,14 +53,19 @@ def get_images(content):
             tmp = re.sub(r'"', "", prefix_ret)
             tmp = re.sub(r' ', "", tmp)
             images.append(tmp)
-
-    images = list(set(images))
     return images
 
+def get_image_dict(target,images):
+    ret_list = list(
+        map(lambda x: {'s_image':x,'t_image':to_target_image(target,x)},images)
+    )
+    return ret_list
 
+def to_target_image(target,i):
+    return os.path.join(target,to_generial_image(i))
 
 def to_generial_image(i):
-    i.split("/")[-1].split("@")[0]
+    return i.split("/")[-1].split("@")[0]
 
 if __name__ == '__main__':
     #! Loop install file for request, and then get all image.
@@ -68,7 +79,8 @@ if __name__ == '__main__':
         if install_yaml.status_code != 200:
             print(f"request error:{install_yaml.status_code}:{install_file.url}")
         else:
-            ConfigYaml.expand_images(get_images(install_yaml.content))
+            ret_images = get_image_dict(install_file.target_registry,get_images(install_yaml.content))
+            ConfigYaml.expand_images(ret_images)
             ConfigYaml.add_install_file(install_file.url)
 
     ConfigYaml.save_yaml()
