dd3bef5c0466e483db83dc11542d8c8286d5db4e
huang12zheng <805104533@qq.com>
2022-06-24 12:20:35 +0800

    fix: handle local pull

--- pull_to_local.py
+++ pull_to_local.py
@@ -10,11 +10,11 @@ if __name__ == '__main__':
         #! run docker command
         print("[GetImages] {}".format(image))
         saveImage=re.sub("@.*","",image["s_image"])
-        docker_pull_cmd = "docker pull {0}".format(image["t_image"])
-        docker_tag_cmd = "docker tag {0} {1}".format(image["t_image"], saveImage)
-        docker_rm_cmd = "docker rmi {0}".format(image["t_image"])
+        docker_pull_cmd = "sudo docker pull {0}".format(image["t_image"])
+        docker_tag_cmd = "sudo docker tag {0} {1}".format(image["t_image"], saveImage)
+        docker_rm_cmd = "sudo docker rmi {0}".format(image["t_image"])
 
-        docker_save_cmd = f"docker save {saveImage} > ~/.docker_image.tmp.tar"
+        docker_save_cmd = f"sudo docker save {saveImage} > ~/.docker_image.tmp.tar"
         ctr_image_import = f'{import_command} ~/.docker_image.tmp.tar'
 
         # docker_push_cmd = "docker push {0}".format(image["t_image"])
